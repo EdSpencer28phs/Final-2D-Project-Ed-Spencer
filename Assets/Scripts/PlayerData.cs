021654608c8dc60bc6b47726ba3397996d0b56ab
EdSpencer28phs~ <Edward.Spencer@pallottihs.info>
2026-04-21 09:16:14 -0400

    <April 21>

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public float HP;
+    public float XP;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        HP = 100;
+        XP = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
