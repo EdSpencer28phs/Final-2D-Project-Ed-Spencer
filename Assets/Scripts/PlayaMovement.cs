d5a22bf3e2209a3336c312d1f2964ab5d633df34
EdSpencer28phs~ <Edward.Spencer@pallottihs.info>
2026-04-29 13:53:05 -0400

    <New scenes, hazards added>

--- Assets/Scripts/PlayaMovement.cs
+++ Assets/Scripts/PlayaMovement.cs
@@ -7,6 +7,8 @@ public class PlayaMovement : MonoBehaviour
     public Rigidbody2D rb;
     private float horizontalInput;
     private float verticalInput;
+    public PlayerData data;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,18 +18,24 @@ public class PlayaMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+        
     }
     void FixedUpdate()
     {
         //why aren't you working
-        rb.position += Vector2.right * horizontalInput * moveSpeed * Time.fixedDeltaTime;
-        rb.position += Vector2.up * verticalInput * moveSpeed * Time.fixedDeltaTime;
-        if (Input.GetKey(KeyCode.LeftControl))
+        if(data.isAlive == true)
         {
-            rb.position += Vector2.right * horizontalInput * moveSpeed * 2 * Time.fixedDeltaTime;
-            rb.position += Vector2.up * verticalInput * moveSpeed * 2 * Time.fixedDeltaTime;
+            rb.position += Vector2.right * horizontalInput * moveSpeed * Time.fixedDeltaTime;
+            rb.position += Vector2.up * verticalInput * moveSpeed * Time.fixedDeltaTime;
+            if (Input.GetKey(KeyCode.LeftControl))
+            {
+                rb.position += Vector2.right * horizontalInput * moveSpeed * 2 * Time.fixedDeltaTime;
+                rb.position += Vector2.up * verticalInput * moveSpeed * 2 * Time.fixedDeltaTime;
+            }
         }
     }
 }
--- Assets/Scripts/PlayerData.cs
+++ Assets/Scripts/PlayerData.cs
@@ -4,16 +4,34 @@ public class PlayerData : MonoBehaviour
 {
     public float HP;
     public float XP;
+    public Rigidbody2D rb;
+    public bool isAlive;
+    public PlayaMovement data;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         HP = 100;
         XP = 0;
+        isAlive = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(HP <= 0)
+        {
+            Debug.Log("You Died :(");
+            isAlive = false;
+        }
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Hazard")
+        {
+           //Subtracts 10 HP
+            HP += -10;
+            Debug.Log("Ouch");
+        }
     }
 }
